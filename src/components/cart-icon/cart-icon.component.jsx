b8918678ddae18c2617692e9d2b099ee7df641fd
AYUSH PRAKASH PANDEY <85828536+ayushpandey2002@users.noreply.github.com>
2023-05-16 13:07:49 +0530

    add item and remove item

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemCount} from '../../redux/cart/cart.selectors'
+
+import {createStructuredSelector} from 'reselect';
 
 import {ReactComponent as ShoppingIcon } from '../../assets/122 shopping-bag.svg'
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden,itemCount}) => (
     <div className="cart-icon" onClick ={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
         <span className="item-count">
-            0
+            {itemCount}
         </span>
     </div>
 );
@@ -20,7 +23,11 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
+const mapStateToProps = createStructuredSelector ({
+    itemCount : selectCartItemCount
+})
+
 export default connect
-    (null,
+    (mapStateToProps,
     mapDispatchToProps)
     (CartIcon); 
